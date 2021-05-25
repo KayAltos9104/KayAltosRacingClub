fd5a383895ab42b3e075499f3901d1229fadc5b8
Kay Altos <kayaltosnexus@gmail.com>
2021-05-25 17:45:45 +0300

    Начал делать новую объектную модель

---
+++ KARC/KARC/Game1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace KARC
+{
+    
+    enum GameMode
+    {
+        mainMenu,
+        game,
+        final
+    }
+
+    
+
+
+    public class Game1 : Game
+    {
+        Dictionary<string, Texture2D> texturesDict;
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        GameMode mode;
+
+        Logic.Object back;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            texturesDict = new Dictionary<string, Texture2D>();
+            mode = GameMode.mainMenu;
+        }
+
+      
+
+
+        protected override void Initialize()
+        {         
+            base.Initialize();
+            
+        }
+
+
+
+        
+        protected override void LoadContent()
+        {
+            
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            back = new Logic.Object(Vector2.Zero,1.0f);
+
+            texturesDict.Add("InitialBackGround", Content.Load<Texture2D>("MenuBackGround"));
+
+            back.loadImages("Back", texturesDict["InitialBackGround"]);
+            
+        }
+
+       
+        protected override void UnloadContent()
+        {
+           
+        }
+
+      
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            switch (mode)
+            {
+                case GameMode.mainMenu:
+                    {
+
+                        break;
+                    }
+            }
+
+            base.Update(gameTime);
+        }
+
+
+
+      
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+
+            back.Update(spriteBatch);
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ KARC/KARC/Logic/Object.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KARC.Logic
+{
+    class Object
+    {
+        Dictionary<string, Texture2D> images = new Dictionary<string, Texture2D>();//Изображения, которые имеет объект
+        Texture2D currentImage;
+        Vector2 pos;
+
+        float layer = 1.0f; //Слой отрисовки
+
+        public Object()
+        {
+
+        }
+
+        public Object (Vector2 _pos, float _layer)
+        {
+            layer = _layer;
+            pos = _pos;
+            
+        }
+
+
+        public void loadImages(string _key, Texture2D _image)
+        {
+            images.Add(_key, _image);
+        }
+
+        protected virtual void drawObject (SpriteBatch _spriteBatch)
+        {
+            _spriteBatch.Draw(currentImage,pos,null, Color.White,0, Vector2.Zero,1.0f, SpriteEffects.None,layer);
+        }
+
+        public virtual void Update (SpriteBatch _spriteBatch)
+        {
+            currentImage = images.ElementAt(0).Value;
+            drawObject(_spriteBatch);
+        }
+
+        public virtual void Update(SpriteBatch _spriteBatch, Keys key)
+        {
+            drawObject(_spriteBatch);
+        }
+
+
+    }
+}

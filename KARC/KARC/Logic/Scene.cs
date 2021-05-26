a552be50bdbba5665a56634aa52cacaf7b2aff1a
Kay Altos <kayaltosnexus@gmail.com>
2021-05-26 17:44:02 +0300

    Добавил класс сцены

---
+++ KARC/KARC/Logic/Scene.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KARC.Logic
+{
+    class Scene //Класс, который содержит в себе объекты и карту их расстановки
+    {
+        List<Object> objectList;//Список всех объектов сцены
+        int[,] map;//Клетки карты (одна клетка, по идее, один экран)
+        int scale;// Масштаб одного тайла(клетки) карты
+
+        public Scene (int [,] _map, int _scale, List<Object> _objList)
+        {
+            map = new int[_map.GetLength(0), _map.GetLength(1)];
+            for (int y = 0; y < map.GetLength(1); y++)
+                for (int x = 0; x < map.GetLength(0); x++)
+                    map[x, y] = _map[x, y];
+
+            scale = _scale;
+            objectList = new List<Object>();
+            foreach (var obj in _objList)
+                objectList.Add(obj);
+        }
+
+        public void updateScene (SpriteBatch _spriteBatch, Keys key, int _time)
+        {
+            foreach (var obj in objectList)
+                obj.Update(_spriteBatch, key, _time);
+        }
+
+    }
+}

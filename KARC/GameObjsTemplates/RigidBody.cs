1b6dc730e88303318d1605e3ce6e79c978f2ef4f
Kay <kayaltosnexus@gmail.com>
2021-10-24 00:21:14 +0300

    Добавил класс RigidBody для физических объектов

---
+++ KARC/GameObjsTemplates/RigidBody.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KARC.GameObjsTemplates
+{
+    class RigidBody
+    {
+        private Rectangle _hitBox;        
+        private int _weight;//Вес объекта в килограммах
+
+        public RigidBody (int weight, Point p, Point size)
+        {
+            _hitBox = new Rectangle(p, size);
+            _weight = weight;
+        }
+        public bool IsCollided (Rectangle anotherHitBox)
+        {
+            return _hitBox.Intersects(anotherHitBox);
+        }
+    }
+}
